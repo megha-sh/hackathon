d80b80cdfeed084bec8dedafc084c18b2b23f577
megha-sh <108064593+megha-sh@users.noreply.github.com>
2025-04-23 19:18:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -99,4 +99,4 @@ def download_file(filename):
     return send_from_directory(UPLOAD_FOLDER, filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
